432b57c42bf62b7d9e9d4be8781a093322df0107
Jose Antonio <joseantonio.sanchezf26@gmail.com>
2025-04-07 18:03:57 +0200

    Icono de marcado de animales

---
+++ Assets/Scripts/Animals/RegisteredIcon.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class RegisteredIcon : MonoBehaviour
+{
+    public SpriteRenderer registeredIcon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detectar el clic derecho sobre el objeto
+        if (Input.GetMouseButtonDown(2)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            // Si el clic es sobre este objeto
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                ChangeColor();
+            }
+        }
+    }
+    private void ChangeColor()
+    {
+        if (registeredIcon != null)
+        {
+            if( registeredIcon.color == Color.red)
+            {
+                registeredIcon.color = Color.green; // Cambia a blanco
+            }
+            else
+            {
+                registeredIcon.color = Color.red; // Cambia a rojo
+            }
+        }
+    }
+}
